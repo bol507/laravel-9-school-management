dd87de6320f355163b64dfe9c4211efbdd8ac888
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-07-31 13:07:38 -0500

    feat: create dashboard page

---
+++ src/resources/views/admin/main.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="{{asset('backend/images/favicon.ico')}}">
+  <title>School Management System</title>
+  <!-- Vendors Style-->
+	<link rel="stylesheet" href="{{asset('backend/css/vendors_css.css')}}">
+	  
+	<!-- Style-->  
+	<link rel="stylesheet" href="{{asset('backend/css/style.css')}}">
+	<link rel="stylesheet" href="{{asset('backend/css/skin_color.css')}}">
+</head>
+<body class="hold-transition dark-skin sidebar-mini theme-primary fixed">
+
+  @include('admin.body.header')
+  @include('admin.body.sidebar')
+  <!-- content page -->
+  @yield('admin')
+
+  @include('admin.body.footer')
+
+  <!-- Vendor JS -->
+	<script src="{{asset('backend/js/vendors.min.js')}}"></script>
+  <script src="{{asset('assets/icons/feather-icons/feather.min.js')}}"></script>	
+	<script src="{{asset('assets/vendor_components/easypiechart/dist/jquery.easypiechart.js')}}"></script>
+	<script src="{{asset('assets/vendor_components/apexcharts-bundle/irregular-data-series.js')}}"></script>
+	<script src="{{asset('assets/vendor_components/apexcharts-bundle/dist/apexcharts.js')}}"></script>
+	
+	<!-- Sunny Admin App -->
+	<script src="{{asset('backend/js/template.js')}}"></script>
+	<script src="{{asset('backend/js/pages/dashboard.js')}}"></script>
+</body>
+</html>
