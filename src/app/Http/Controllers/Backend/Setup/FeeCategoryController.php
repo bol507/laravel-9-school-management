6af782b6911ef1842ff05f1d140e41d305b7e019
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-08-17 15:01:42 -0500

    feat: fee category model and controller start up files

---
+++ src/app/Http/Controllers/Backend/Setup/FeeCategoryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Backend\setup;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FeeCategoryController extends Controller
+{
+    //
+}
